66c075434b155fdb866cb05e69772eadcc290717
Marcel Frueh <Marcel.Frueh@mailbox.org>
2023-01-23 17:59:41 +0100

    Issues done for 2

--- 02-qa/exam.py
+++ 02-qa/exam.py
@@ -1,9 +1,15 @@
+import datetime
+
 from teacher import Teacher
 
 class Exam:
-    def __init__(self, student_view, answers):
+    def __init__(self, student_view, answers, store_test=False):
         self.student_view = student_view
         self.answers = answers
+
+        if store_test:
+            self.store_test()
+
     
     def take(self):
         answers = {}
@@ -18,15 +24,29 @@ class Exam:
         for question, answer in answers.items():
             if answer.upper() == self.answers[question].upper()[16]:
                 correct_answers+=1
-        return 100 * correct_answers / len(answers)
+        grade = 100 * correct_answers / len(answers)
 
+        if grade < 60:
+            passed = "Not passed!"
+        else:
+            passed = "Passed!"
+        return f"{correct_answers} out of {len(answers)} correct! You achieved: {grade} % : {passed}"
+
+
+    def store_test(self):
+        with open(f'Test_{datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.txt', "w") as file:
+            for question, question_view in self.student_view.items():
+                file.write(question_view)
+                file.write("\n")
+                file.write(self.answers[question])
+                file.write("\n")
 
 
 if __name__ == "__main__":
     teacher = Teacher()
     student_view, answers = teacher.create_full_test()
 
-    exam = Exam(student_view, answers)
+    exam = Exam(student_view, answers, store_test=True)
     student_answers = exam.take()
     print(student_answers)
     grade = exam.grade(student_answers)
